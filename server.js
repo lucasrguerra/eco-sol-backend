623815199fa29970b6d5e14038ee91a728f935be
lucasrguerra <lucasrayanguerra24@gmail.com>
2021-10-03 17:43:23 -0300

    Version 1.1.2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const middleware = require('express-formidable');
 
@@ -9,5 +10,6 @@ const { router } = require('./router');
 
 server.use(middleware());
 server.use(router);
+server.use(cors());
 
 server.listen(process.env.PORT);
